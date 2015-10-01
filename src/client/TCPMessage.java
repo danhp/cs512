6c82bdd59a41622084c4abaea1d541d445583844
justindomingue <justin.domingue@hotmail.com>
2015-10-01 18:56:29 -0400

    filled the proxy

--- src/client/TCPMessage.java
+++ src/client/TCPMessage.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 public class TCPMessage implements Serializable {
     public int id;
     public int type; //0=ack,1=msg
-    public int itemType;   //0=car 1=flight 2=room 3=itinerary
+    public int itemType;   //0=car 1=flight 2=room 3=customer 4=itinerary
     public int actionType; //0=get 1=add 2=delete 3=reserve
 
     // specific to reservable item
